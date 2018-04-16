465d82cfff1c60a4b194379156e7f32215d9e0ac
Lucas Alias <lucas@LUCAS3>
2018-04-16 09:55:39 +0200

    First Commit

---
+++ header.php
@@ -0,0 +1,28 @@
+<div class="index-layout-transparent mdl-layout mdl-js-layout">
+    <header class="mdl-layout__header mdl-layout__header--transparent mdl-layout__header--waterfall">
+        <div class="mdl-layout__header-row">
+            <!-- Title -->
+            <span class="mdl-layout-title">GraphCommunity</span>
+            <!-- Add spacer, to align navigation to the right -->
+            <div class="mdl-layout-spacer"></div>
+            <!-- Navigation -->
+            <nav class="mdl-navigation">
+                <a class="mdl-navigation__link" href="">Accueil</a>
+                <a class="mdl-navigation__link" href="">Portfolio</a>
+                <a class="mdl-navigation__link" href="">Link</a>
+                <a class="mdl-navigation__link" href="">Link</a>
+            </nav>
+        </div>
+    </header>
+    <div class="mdl-layout__drawer">
+        <span class="mdl-layout-title">GraphCommunity</span>
+        <nav class="mdl-navigation">
+            <a class="mdl-navigation__link" href="">Accueil</a>
+            <a class="mdl-navigation__link" href="">Portfolio</a>
+            <a class="mdl-navigation__link" href="">Link</a>
+            <a class="mdl-navigation__link" href="">Link</a>
+        </nav>
+    </div>
+    <main class="mdl-layout__content">
+    </main>
+</div>
