4219d685b0edea4eebe82936f24651e4f42ce471
Lucas Alias <lucas@LUCAS3>
2018-04-18 16:12:08 +0200

    update

--- index.php
+++ index.php
@@ -8,14 +8,15 @@
 		<title>GraphCommunity</title>
 
 		<script src="https://code.getmdl.io/1.3.0/material.min.js"></script>
-		<link rel="stylesheet" href="http://fonts.googleapis.com/css?family=Roboto:300,400,500,700" type="text/css">
+		<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:regular,bold,italic,thin,light,bolditalic,black,medium&amp;lang=fr">
 		<link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
 		<link rel="stylesheet" href="https://code.getmdl.io/1.3.0/material.indigo-deep_purple.min.css" />
 
 		<link rel="stylesheet" href="asset/css/layout.css" type="text/css">
 	</head>
 
-	<body>
+	<body class="mdl-base">
+		<?php include("header.php"); ?>
   </body>
 
 </html>
